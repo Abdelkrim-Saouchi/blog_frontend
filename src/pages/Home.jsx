f7f363c98bebe697f578f706ce9ac56dbbb7a9b3
Krimothiazine <krimouv1995@gmail.com>
2024-02-02 17:21:22 +0100

    Create first look home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <div>Hello world</div>;
+  return (
+    <main className="px-4 py-2 pt-4">
+      <div className="flex items-center gap-6 border-b border-gray-200 pb-4 opacity-70">
+        <span className="icon-[ph--plus-thin]"></span>
+        <div className="font-bold">For you</div>
+        <div>Topics</div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
