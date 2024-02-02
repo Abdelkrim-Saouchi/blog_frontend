d5b697ae6e79dab8b3dc21bbb51ad92b105adaef
Krimothiazine <krimouv1995@gmail.com>
2024-02-02 09:10:09 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="px-4 font-bold">
+      Hello world<span className="icon-[material-symbols--10mp]"></span>
+    </h1>
+  );
+}
+
+export default App;
