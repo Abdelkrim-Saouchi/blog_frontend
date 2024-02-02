75477c1a40e334d67e74d61052703743e17221d8
Krimothiazine <krimouv1995@gmail.com>
2024-02-02 17:05:36 +0100

    Refactor to use react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import Header from "./components/Header";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./components/Root";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <Header />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="flex flex-row items-center justify-between gap-4 border-b border-gray-200 px-4 py-2">
-      <span className="icon-[game-icons--bookmarklet] text-4xl"></span>
+      <Link to="/">
+        <span className="icon-[game-icons--bookmarklet] text-4xl"></span>
+      </Link>
       <div className="flex items-center rounded-lg border border-gray-100 bg-gray-100 p-2">
         <span className="icon-[mdi--search] text-2xl text-gray-600"></span>
         <input
@@ -13,14 +17,14 @@ const Header = () => {
       <nav>
         <ul className="flex gap-4 font-semibold">
           <li>
-            <a href="#" className="hover:opacity-70 ">
+            <Link to="/" className="hover:opacity-70 ">
               Login
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="hover:opacity-70 ">
+            <Link to="/" className="hover:opacity-70 ">
               Sign up
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
