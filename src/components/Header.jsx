a0b231d22e073c15d1990219ae74f5886b50d49b
Krimothiazine <krimouv1995@gmail.com>
2024-02-02 16:38:43 +0100

    Finish first look of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,30 @@
 const Header = () => {
   return (
-    <div className="flex flex-row items-center justify-between gap-4 px-4 py-2">
+    <header className="flex flex-row items-center justify-between gap-4 border-b border-gray-200 px-4 py-2">
       <span className="icon-[game-icons--bookmarklet] text-4xl"></span>
       <div className="flex items-center rounded-lg border border-gray-100 bg-gray-100 p-2">
         <span className="icon-[mdi--search] text-2xl text-gray-600"></span>
         <input
           type="search"
-          className="bg-gray-100 outline-none focus:outline-none"
+          placeholder="Search"
+          className="bg-gray-100 pl-3 outline-none focus:outline-none"
         />
       </div>
-    </div>
+      <nav>
+        <ul className="flex gap-4 font-semibold">
+          <li>
+            <a href="#" className="hover:opacity-70 ">
+              Login
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:opacity-70 ">
+              Sign up
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
