248f368e51162fac85c2449ae4a5dc3bc374df4f
Krimothiazine <krimouv1995@gmail.com>
2024-02-16 21:27:40 +0100

    Refactor comment creation date

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,7 +5,7 @@ const Comment = ({ comment }) => {
     <div className="rounded border border-gray-200 p-3">
       <div className="flex gap-2 text-lg">
         <div className="font-bold">{comment.author.username}</div>
-        <div className="text-gray-500">{comment.createdAt}</div>
+        <div className="text-gray-500">{comment.creationDate}</div>
       </div>
       <div>{comment.content}</div>
     </div>
