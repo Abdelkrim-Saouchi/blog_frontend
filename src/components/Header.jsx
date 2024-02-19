981ae1e45546b3bc27f7d157c0a139d3bdfffe0f
Krimothiazine <krimouv1995@gmail.com>
2024-02-19 19:30:21 +0100

    Fix mobile menu bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,33 @@
 import PropTypes from "prop-types";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useFetcher } from "react-router-dom";
 
 const Header = ({ token }) => {
   const menuRef = useRef(null);
   const fetcher = useFetcher();
+  const [open, setOpen] = useState(false);
 
   const toggleMenu = () => {
-    menuRef.current.classList.toggle("hidden");
+    // menuRef.current.classList.toggle("hidden");
+    setOpen((prev) => !prev);
   };
+
+  const handleOutsideClick = (e) => {
+    if (menuRef.current && !menuRef.current.contains(e.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("touchend", handleOutsideClick);
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    () => {
+      document.removeEventListener("touchend", handleOutsideClick);
+      document.removeEventListener("mousedown", handleOutsideClick);
+      return;
+    };
+  }, []);
 
   return (
     <header className="flex flex-row items-center justify-between gap-4 border-b border-gray-200 px-2 py-2 md:px-4">
@@ -57,33 +76,35 @@ const Header = ({ token }) => {
           className="icon-[mdi--hamburger-menu] text-3xl"
         ></span>
       </div>
-      <nav
-        ref={menuRef}
-        className="absolute right-4 top-10 z-10 rounded border border-slate-200 bg-white px-4 py-4 shadow-md md:hidden "
-      >
-        <ul className="flex flex-col gap-4 font-semibold">
-          {!token && (
-            <>
+      {open && (
+        <nav
+          ref={menuRef}
+          className="absolute right-4 top-10 z-10 rounded border border-slate-200 bg-white px-4 py-4 shadow-md md:hidden "
+        >
+          <ul className="flex flex-col gap-4 font-semibold">
+            {!token && (
+              <>
+                <li onClick={toggleMenu} className="p-2 hover:bg-slate-200">
+                  <Link to="/login">Login</Link>
+                </li>
+                <li onClick={toggleMenu} className="p-2 hover:bg-slate-200">
+                  <Link to="/singup">Sign up</Link>
+                </li>
+              </>
+            )}
+            {token && (
               <li onClick={toggleMenu} className="p-2 hover:bg-slate-200">
-                <Link to="/login">Login</Link>
+                <fetcher.Form method="post" action="/">
+                  <button className="flex items-center gap-1 hover:opacity-70">
+                    Logout
+                    <span className="icon-[mdi--exit-run] text-xl"></span>
+                  </button>
+                </fetcher.Form>
               </li>
-              <li onClick={toggleMenu} className="p-2 hover:bg-slate-200">
-                <Link to="/singup">Sign up</Link>
-              </li>
-            </>
-          )}
-          {token && (
-            <li onClick={toggleMenu} className="p-2 hover:bg-slate-200">
-              <fetcher.Form method="post" action="/">
-                <button className="flex items-center gap-1 hover:opacity-70">
-                  Logout
-                  <span className="icon-[mdi--exit-run] text-xl"></span>
-                </button>
-              </fetcher.Form>
-            </li>
-          )}
-        </ul>
-      </nav>
+            )}
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
