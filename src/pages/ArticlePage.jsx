de40a4f98394b6f6ceb546236b2a4b3f567dae24
Abdelkrim Saouchi <krimouv1995@gmail.com>
2024-04-04 23:49:20 +0100

    use tailwind typography to fix code block breaking screen

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -74,7 +74,7 @@ const ArticlePage = () => {
         )}
         <div
           dangerouslySetInnerHTML={{ __html: article.content }}
-          className="pose-pre:w-full prose prose-sm max-w-full scroll-smooth leading-relaxed md:prose-base prose-code:w-full prose-pre:overflow-x-auto"
+          className="pose-pre:w-full prose prose-sm max-w-full leading-relaxed md:prose-base prose-code:w-full prose-pre:overflow-x-auto prose-pre:scroll-smooth"
         ></div>
         <div className="my-8">
           {article.topics.map((topic) => (
