18c25ba8b38626613b8d8e938c3e055dd27e4452
Abdelkrim Saouchi <krimouv1995@gmail.com>
2024-04-04 22:27:57 +0100

    Remove padding on loggout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -119,7 +119,7 @@ const Header = ({ token }) => {
                     <span className="icon-[ph--note-pencil-thin] text-xl"></span>
                   </a>
                 </li>
-                <li className="p-2 hover:bg-slate-200">
+                <li className="hover:bg-slate-200">
                   <fetcher.Form method="post" action="/">
                     <button className="flex items-center gap-1 hover:opacity-70">
                       Logout
