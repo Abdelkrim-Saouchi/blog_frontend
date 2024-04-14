756b432531eee6f0a18bcba77e9e76cd7c89e16b
Abdelkrim Saouchi <krimouv1995@gmail.com>
2024-04-14 22:01:58 +0100

    Change colorscheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        "custom-text": "#05140c",
-        "custom-bg": "#effbf6",
-        "custom-primary": "#3bce7d",
-        "custom-primary-light": "hsl(187.06deg 65.38% 89.8%)",
-        "custom-secondary": "#b698e6",
-        "custom-secondary-light": "#eaeff2",
-        "custom-accent": "#cc5fd8",
+        "custom-text": "#032009",
+        "custom-bg": "#F2FEF4",
+        "custom-primary": "#1CEC50",
+        "custom-secondary": "#8890F5",
+        "custom-accent": "#A148EF",
       },
     },
   },
